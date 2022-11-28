43a0e0f0c2b1e70da3884dcf80a62f0597e2c75e
Jonathan Crum <jcrum@theobogroup.com>
2022-11-28 11:16:02 -0500

    Updating boilerplate

--- calculator/main.py
+++ calculator/main.py
@@ -1,16 +1,24 @@
 from __future__ import annotations
 from beartype.typing import *
-from typing import TypeAlias
-from typing_extensions import NoReturn
-
-if TYPE_CHECKING:
-    pass
-
-import sys
 
 
-def main() -> NoReturn:
-    sys.exit()
+class Calculator:
+
+    @staticmethod
+    def add(input: str = "") -> int:
+        """
+        Add together a string representation of comma-separated list of 
+        integers.
+        """
+        if input:
+            pass
+        return 0
+
+
+def main(input: str = "") -> None:
+    """Application entrypoint."""
+    result = Calculator.add(input)
+    print(result)
 
 
 if __name__ == '__main__':
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,10 @@
+from calculator.main import main
+import pytest
+
 
 it_works = True
 
 
 def test_basic_testing():
     assert it_works == True
+    main("1, 2, 3")
