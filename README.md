3ca7fa003d825cb44dbd4668ed393971337266ab
Jonathan Crum <jcrum@theobogroup.com>
2022-11-28 09:59:55 -0500

    boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Test Driven Development (TDD) Example Application
+
+1. What is TDD?
+2. What is SOLID?
+3. Example Application
+   1. Boilerplate Code
+   2. Writing Tests
+   3. Satisfying Tests
+4. Github Actions
+5. Coverage Testing
---
+++ calculator/main.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from beartype.typing import *
+from typing import TypeAlias
+from typing_extensions import NoReturn
+
+if TYPE_CHECKING:
+    pass
+
+import sys
+
+
+def main() -> NoReturn:
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
