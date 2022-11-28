3bd7d8c726ce8b2b5e2ddb9c0fbe0d802df0acb3
Jonathan Crum <jcrum@theobogroup.com>
2022-11-28 11:29:53 -0500

    part-1

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,9 +2,13 @@ from calculator.main import main
 import pytest
 
 
-it_works = True
-
-
-def test_basic_testing():
-    assert it_works == True
+def test_too_many_arguments_exception():
     main("1, 2, 3")
+
+
+def test_two_arguments_no_exception():
+    main("1, 2")
+
+
+def test_non_integer_argument_exception():
+    main("1, X")
